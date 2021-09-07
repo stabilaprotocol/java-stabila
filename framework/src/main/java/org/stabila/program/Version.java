70428a891f52b9fbe1bc18155db65b2b377c7f76
dantheman1978 <daniel.varzari@gmail.com>
2021-09-07 13:54:18 +0300

    112

---
+++ framework/src/main/java/org/stabila/program/Version.java
@@ -0,0 +1,12 @@
+package org.stabila.program;
+
+public class Version {
+
+  public static final String VERSION_NAME = "GreatVoyage-v4.2.2.1-281-gc1d9dfd6c";
+  public static final String VERSION_CODE = "15872";
+  private static final String VERSION = "4.3.0";
+
+  public static String getVersion() {
+    return VERSION;
+  }
+}
